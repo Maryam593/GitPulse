3cc58aed41ea2d72e3ae17bca548033ed3fd0bb4
Maryam Saba <maryams91101@gmail.com>
2025-07-11 13:48:41 +0500

    vercel route setting

--- server/server.js
+++ server/server.js
@@ -6,7 +6,12 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://git-pulse.vercel.app', // Vercel site ka origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // API Route to fetch GitHub stats
